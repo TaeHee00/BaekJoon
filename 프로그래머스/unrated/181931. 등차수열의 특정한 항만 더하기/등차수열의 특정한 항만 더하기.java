3ecb9b73f4c2edb3f7dc6aa513cba1b421e702a0
Tae Hee Jang <107194331+TaeHee00@users.noreply.github.com>
2023-06-04 21:06:05 +0900

    [unrated] Title: 등차수열의 특정한 항만 더하기, Time: 0.03 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int a, int d, boolean[] included) {
+        int answer = 0;
+        
+        for (int i = 0; i < included.length; i++) {
+            // included가 참인지 확인
+            if (included[i]) {
+                answer += a;
+            }
+            a += d;
+        }
+        
+        return answer;
+    }
+}
