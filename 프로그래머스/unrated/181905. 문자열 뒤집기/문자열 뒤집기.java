eab0776e9e0dab88db7c1b8ae34efd627ec4836a
Tae Hee Jang <107194331+TaeHee00@users.noreply.github.com>
2023-06-12 11:46:09 +0900

    [unrated] Title: 문자열 뒤집기, Time: 12.62 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        String answer = "";
+        StringBuffer sb = new StringBuffer(my_string.substring(s, e + 1));
+        answer = my_string.substring(0, s) + sb.reverse().toString() + my_string.substring(e + 1);
+        return answer;
+    }
+}
