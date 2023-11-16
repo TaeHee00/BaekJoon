4b5e2dd6c810ccda4453cc002fc73b866195d21a
Tae Hee Jang <107194331+TaeHee00@users.noreply.github.com>
2023-11-16 19:22:32 +0900

    [Silver III] Title: 2×n 타일링, Time: 80 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,25 +1,23 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static final HashMap<Integer, Integer> dp = new HashMap<>();
+    static int[] dp = new int[1001];
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
-
-        dp.put(1, 1);
-        dp.put(2, 2);
-        dp.put(3, 3);
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
 
         for (int i = 4; i <= N; i++) {
-            dp.put(i, (dp.get(i - 1) + dp.get(i - 2)) % 10007);
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
         }
 
-        System.out.println(dp.get(N));
+        System.out.println(dp[N]);
     }
 }
